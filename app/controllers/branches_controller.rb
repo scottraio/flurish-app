37f0b92cea0121f73dd32a65e12a71e4b74fd6c9
Brandon Dek <bdek@fraktalicious.(none)>
2010-01-19 19:37:21 -0800

    branches

--- app/controllers/branches_controller.rb
+++ app/controllers/branches_controller.rb
@@ -1,21 +1,24 @@
 class BranchesController < ApplicationController
   
+  before_filter :set_user, 				:only 		=> [:new, :create]
   before_filter :get_user, 				:only 		=> [:destroy, :update, :show, :edit]
   	
+  def new
+  end
+  
+  def create
+  end	
+  	
   def show
-  
   end
   
-  def edit
-    
-  end
+  def edit 
+  end 
   
   def update
-    
   end
   
-  def destroy
-    
+  def destroy 
   end
 
 private
