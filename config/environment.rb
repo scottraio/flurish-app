195f1abcc6a0046e5e55874204ce087961967f35
Scott Raio <scottraio@gmail.com>
2010-02-11 18:22:07 -0800

    memcached issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,7 @@
-require "memcache"
-
+require "memcached"
 # Be sure to restart your server when you modify this file
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -21,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-	config.gem "memcached"
+	config.gem "memcached" 
 	config.gem "paperclip"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
@@ -47,15 +46,7 @@ Rails::Initializer.run do |config|
 	# Memcached
 	#
 
-	CACHE 				= MemCache.new(:namespace => "flurish")
-	CACHE.servers = '127.0.0.1:11211'
+  config.action_controller.session_store = :mem_cache_store
+
 	
-	config.action_controller.session_store = :mem_cache_store
-	
-	config.action_controller.session = {
-		:session_key 	=> 	'_flurish',
-		:secret      	=> 	'smashbrosisthegreatestgameever',
-		:cache       	=> 	CACHE,
-		:expires			=>	900  
-	}
 end
