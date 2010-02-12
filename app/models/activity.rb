e2c7b888af74bd630ba0d4f8923e43dc26d92170
Scott Raio <scottraio@gmail.com>
2010-02-12 13:00:46 -0800

    fixed feed bug where user is not following any branches

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,6 +6,20 @@ class Activity < ActiveRecord::Base
 	belongs_to	:element
 	
 	has_many		:comments, :as => :commentable
+	
+	def self.sql(kind,user)
+		case kind
+			when :conditions then build_conditions_sql(user)
+		end
+	end
+	
+	def self.build_conditions_sql(user)
+		# include all leaders
+		sql = 	"activities.created_by IN (#{(user.leaders.collect{|u| u.id } << user.id).uniq.join(",")})" 
+		# only include branch activity for the stuff we're following, do not run this
+		# sql statement if the user is not following any branches
+		sql += 	"OR activities.branch_id IN (#{user.branches.collect{|b| b.id }.uniq.join(",")})" unless user.branches.empty?
+	end
 
 end
 
