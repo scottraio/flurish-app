9f096adf19db2978f4f9c092eb5fd1b078d8efc8
Scott Raio <scottraio@gmail.com>
2010-02-24 15:00:14 -0800

    notes element, edit topics form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,46 @@
 class NotesController < ApplicationController
+	
+	before_filter :get_topic_and_element
+	before_filter :set_note, :only => [:new,:create]
+	before_filter :get_note, :only => [:edit,:update]
+	
+	def new
+		render :layout => false
+	end
+	
+	def create
+		if @note.save
+			render_index
+		end
+	end
+	
+	def edit
+		render :layout => false
+	end
+	
+	def update
+		if @note.save
+			render_index
+		end
+	end
+	
+private
+
+	def render_index
+		render(:partial => "notes/index", :locals => {:element => @element})
+	end
+
+	def get_topic_and_element
+		@topic 		= Topic.get(current_user,params)
+		@element	= Element.find(params[:element_id])
+	end
+	
+	def set_note
+		@note = Note.set(@element,current_user,params)
+	end
+	
+	def get_note
+		@note = Note.get(@element,current_user,params)
+	end
+	
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function init() {
+	$('[rel*=facebox]').unbind('click').facebox();
+	$("div.item div.details").ellipsis();
+	$("div.notice").fadeOut(3000);
+
+	$("a.remote").unbind('click').bind('click', function() { 
+	  if(this.rel) $(this.rel).load(this.href); 
+	  else $.get(this.href); 
+	  return false; 
+	});
+}
+	
