143e96504ef849fe8e08b76890b14338000fabb7
Brandon Dek <bdek@fraktalicious.(none)>
2010-02-24 19:03:26 -0800

    topic details

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -2,9 +2,9 @@ module TopicsHelper
 	
 	def follow_topic
 		if @user.following? @topic
-			link_to "Stop Following", stop_following_topic_path(@topic), :class => "right metal"
+			link_to "Stop Following", stop_following_topic_path(@topic), :class => "left metal"
 		else
-			link_to "Follow", follow_topic_path(@topic), :class =>  "right metal"
+			link_to "Follow", follow_topic_path(@topic), :class =>  "left metal"
 		end
 	end
 	
