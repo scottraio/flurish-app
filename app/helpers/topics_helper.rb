483a43f3ca50717755b09f46168c515ba0541235
Scott Raio <scottraio@gmail.com>
2010-02-24 18:48:59 -0800

    different details box

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,4 +1,11 @@
 module TopicsHelper
 	
+	def follow_topic
+		if @user.following? @topic
+			link_to "Stop Following", stop_following_topic_path(@topic), :class => "right metal"
+		else
+			link_to "Follow", follow_topic_path(@topic), :class =>  "right metal"
+		end
+	end
 	
 end
