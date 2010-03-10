b23575c8f19440b7bd069e9cd17d2f32c06e87d1
Brandon Dek <bdek@fraktalicious.(none)>
2010-03-09 18:42:48 -0800

    links element

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,16 +1,47 @@
 class LinksController < ApplicationController
   
+  before_filter :get_topic_and_element
+  before_filter :set_link, :only => [:new,:create]
+	before_filter :get_link, :only => [:edit,:update]
+  
   def new
 		render :layout => false
 	end
 	
+	def create
+		if @link.save
+		  render_index
+		else
+		  flash[:error] = @link.errors
+			render :new, :layout => false, :status => 500
+		end
+	end
+	
+	def edit
+		render :layout => false
+	end
+	
+	def update
+		if @link.save
+		end
+	end
+	
 	private
+	
+	def render_index
+		render(:partial => "links/index", :locals => {:element => @element})
+	end
+	
+	def get_topic_and_element
+		@topic 		= Topic.get(current_user,params)
+		@element	= Element.find(params[:element_id])
+	end
   
   def get_link
 		@link = Link.get(@element,current_user,params)
 	end
 	
-	def set_note
+	def set_link
 		@link = Link.set(@element,current_user,params)
 	end
   
--- app/models/link.rb
+++ app/models/link.rb
@@ -3,11 +3,16 @@ class Link < ActiveRecord::Base
 	belongs_to 	:creator, :class_name => "User", :foreign_key => "created_by"
 	belongs_to	:element
 	
+	belongs_to 	:creator, 	:class_name => "User", :foreign_key => "created_by"
+	
+	validates_presence_of 	:name
 	validates_presence_of 	:url
+	validates_format_of     :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
 	
 	def self.get(element,user,options={})
 		l 						= self.find(options[:id])
 		l.element 		= element
+		l.creator 		= user
 		l.attributes	= options[:link]
 		l
 	end
@@ -15,6 +20,7 @@ class Link < ActiveRecord::Base
 	def self.set(element,user,options={})
 		l 				= self.new(options[:link])
 		l.element = element
+		l.creator = user
 		l
 	end
 	
