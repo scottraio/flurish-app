ee51e719361079c6b101937565a2d7815ece0df1
Brandon Dek <bdek@fraktalicious.(none)>
2010-03-19 14:55:18 -0700

    task element

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,54 +1,57 @@
 class TasksController < ApplicationController
 
-    before_filter :get_topic_and_element
-    before_filter :set_task, :only => [:new,:create]
-  	before_filter :get_task, :only => [:edit,:update]
+  before_filter :get_topic_and_element
+  before_filter :set_task, :only => [:new,:create]
+	before_filter :get_task, :only => [:edit,:update,:pickup]
 
-    def new
-  		render :layout => false
-  	end
+  def new
+		render :layout => false
+	end
 
-  	def create
-  		if @task.save
-  		  render_index
-  		else
-  		  flash[:error] = @task.errors
-  			render :new, :layout => false, :status => 500
-  		end
-  	end
+	def create
+		if @task.save
+		  render_index
+		else
+		  flash[:error] = @task.errors
+			render :new, :layout => false, :status => 500
+		end
+	end
 
-  	def edit
-  		render :layout => false
-  	end
+	def edit
+		render :layout => false
+	end
 
-  	def update
-  	  if @task.save
-  	    render_index
-  	  end
-  	end
-  	
-  	def pickup
-  	  @task.update_attribute(:assigned_to, current_user.id)
-  	end
-  	
-  	private
+	def update
+	  if @task.save
+	    render_index
+	  end
+	end
+	
+	def pickup
+	  @task.update_attribute(:assigned_to, current_user.id)
+	  render_index
+	end
+	
+	private
 
-  	def render_index
-  		render(:partial => "tasks/index", :locals => {:element => @element})
-  	end
+	def render_index
+		render(:partial => "tasks/index", :locals => {:element => @element})
+	end
 
-  	def get_topic_and_element
-  		@topic 		= Topic.get(current_user,params)
-  		@element	= Element.find(params[:element_id])
-  	end
+	def get_topic_and_element
+		@topic 		= Topic.get(current_user,params)
+		@element	= Element.find(params[:element_id])
+	end
 
-    def get_task
-  		@task = Task.get(@element,current_user,params)
-  	end
+private
 
-  	def set_task
-  		@task = Task.set(@element,current_user,params)
-  	end
+  def get_task
+		@task = Task.get(@element,current_user,params)
+	end
+
+	def set_task
+		@task = Task.set(@element,current_user,params)
+	end
 
   
   
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -17,8 +17,11 @@ module TasksHelper
   end
   
   def due_date(date)
-    date.nil? ? "None" : date.strftime("%m/%d/%Y") 
-
+    date.nil? ? "N/A" : date.strftime("%m/%d/%Y") 
+  end
+  
+  def pickable?(element,task)
+    task.assigned_to.nil? ? (link_to "Pick up", pickup_topic_element_task_path(@topic,element,task), :class => "metal right remote", :rel => "#topic_tasks") : "<div class='grabbed_by'><small>grabbed by:</small>#{task.delegate.first_name + " " + task.delegate.last_name}</div>"
   end
   
 end
